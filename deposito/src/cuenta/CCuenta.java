7f85dec4b9beb7c589ee419d07106342f0d942a1
martin <115216787+martiniuwu@users.noreply.github.com>
2023-02-10 14:19:35 +0100

    Add files via upload

---
+++ deposito/src/cuenta/CCuenta.java
@@ -0,0 +1,42 @@
+package cuenta;
+
+public class CCuenta {
+
+
+    protected String nombre;
+    protected String cuenta;
+    protected double saldo;
+    protected double tipoInteres;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+}
